b76ed096f6681ea91bd3f082f11289d6a5693e77
chiara <marcheschi.chr@gmail.com>
2015-04-09 16:25:21 +0200

    Added a new test in TestUtf8CharOffset

--- src/test/java/com/logentries/re2/TestUtf8CharOffset.java
+++ src/test/java/com/logentries/re2/TestUtf8CharOffset.java
@@ -42,6 +42,8 @@ public class TestUtf8CharOffset {
             "\uD83D\uDC36\uD83D\uDC3Ecd efg", asList(0,0,4,4,8,9,10,11,12,13), asList(0,0,0,0,2,2,2,2,4,5,6,7,8,9)
         },{
             "\uD83D\uDD0D sp", asList(0,0,4,5,6), asList(0,0,0,0,2,3,4)
+        },{
+                "â€", asList(0,2), asList(0,0,1,1,1)
         }});
     }
 
